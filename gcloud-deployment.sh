a75a86c233da1b5b8ef21a06a68c0de1673e1999
Athul <prasanna29387@gmail.com>
2019-08-18 10:54:24 -0400

    changes to push docker images to GCR

--- gcloud-deployment.sh
+++ gcloud-deployment.sh
@@ -5,9 +5,10 @@ MODULES=('discovery-server' 'hazelcast-server' 'hazelcast-client' )
 MODULELIST="discovery-server|hazelcast-server|hazelcast-client"
 THISACTION=$1
 cd $HOME/
-gcloud config configurations activate default
-gcloud auth configure-docker
 echo $id
+#echo gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://gcr.io/
+#cat ke./y.json | docker login -u _json_key --password-stdin https://gcr.io/
+./token.sh
 function executeCommand
 {
     echo "Action is "$THISACTION
@@ -25,14 +26,14 @@ function buildDocker()
 for item in ${MODULES[*]}
 do
 echo $PWD
-echo gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://gcr.io/v2/
+#echo gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://gcr.io/
 cd $HOME/$item
 echo "current directory" $PWD
 echo "building module###################" $item "###########################\n\n\n"
 echo "executing command: docker build -t gcr.io/hazelcast-cluster/$item:v1 -f deploy/Dockerfile ."
-#docker build -t gcr.io/hazelcast-cluster/$item:v1 -f deploy/Dockerfile .
-echo "executing command: gcloud docker -- push  gcr.io/hazelcast-cluster/$item:v1"
-#docker push  gcr.io/hazelcast-cluster/$item:v1
+docker build -t gcr.io/hazelcast-cluster/$item:v3 -f deploy/Dockerfile .
+echo "executing command: gcloud docker -- push  gcr.io/hazelcast-cluster/$item:v3"
+docker push  gcr.io/hazelcast-cluster/$item:v3
 echo "###############################################################\n \n "
 done
 }
---
+++ token.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker login -u _json_key -p "$(cat key.json)" https://gcr.io/
