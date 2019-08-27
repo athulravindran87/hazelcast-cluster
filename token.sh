680c0d698d8bc501e4e142da7a9031269107146a
Athul <prasanna29387@gmail.com>
2019-08-26 20:04:27 -0400

    add features to confif.json

--- token.sh
+++ token.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 whoami
-docker login -u _json_key -p "$(cat ../key.json)" https://gcr.io/
+#docker login -u _json_key -p "$(cat ../key.json)" https://gcr.io/
+cd ~/.docker/
+ls -lart
+cat config.json
+mv config.json config.json_old
+
+ls -lart
